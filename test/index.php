828f8dce9ec85030e4154823b8854540c4faacc5
aldeyf <alp99,54>
2018-09-03 09:11:31 -0300

    Emails config server

--- test/index.php
+++ test/index.php
@@ -5,11 +5,11 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "mail.marioaugustolibras.com.br",
-    "cursos@marioaugustolibras.com.br", "TT@$8.((^iE?", "tls", '587',
-    "cursos@marioaugustolibras.com.br", "Equipe Mario Augusto Libras");
+$novoEmail = new Email(2, "mail.domain.com",
+    "email@example.com", "", "tls", '587',
+    "youremail@domain.com", "Equipe Mario Augusto Libras");
 $novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b>!</p>",
-    "cursos@marioaugustolibras.com.br", "Mario Augusto",
-    "aldeyfreire2@gmail.com", "Aldey");
+    "reply@domain.com.br", "User Name",
+    "fromemail@domain.com", "User Name");
 
 var_dump($novoEmail);
